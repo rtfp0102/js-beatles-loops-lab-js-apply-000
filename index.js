8db2f2492a425728751a28fefc50db7e9dc8b83b
rtfp0102 <rtfp0102@gmail.com>
2019-09-18 00:24:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function theBeatlesPlay(musicians, instruments) {
   return array
 }
 
-function JohnLennonFacts(){
+function johnLennonFacts(){
   var facts = ["He was the last Beatle to learn to drive",
         "He was never a vegetarian",
         "He was a choir boy and boy scout",
