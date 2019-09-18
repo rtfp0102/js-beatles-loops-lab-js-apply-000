25322e6fe68405713de41bd3e41e7e14b712700a
rtfp0102 <rtfp0102@gmail.com>
2019-09-18 00:19:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ function theBeatlesPlay(musicians, instruments) {
   var array = [];
   
   for (var i = 0; i < musicians.length; i += 1) {
-    return musicians[i] +" plays " + instruments[i]
+    array.push(musicians[i] +" plays " + instruments[i])
   }
+  
+  return array
 }
