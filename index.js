fce9d0eab654d5f54bd4cd7b49755822d6d0094f
rtfp0102 <rtfp0102@gmail.com>
2019-09-18 00:13:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
 const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];// add solution here
+const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+function Beatles 
